a95f48446ce1d244c3fd5c35d2e37226cba211c6
Clifton Snyder <cliff.snyder@gmail.com>
2013-02-22 13:10:01 -0500

    adding more ruby examples

--- examples/ruby/hashes.rb
+++ examples/ruby/hashes.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+# create a hash
+books = {} # or Hash.new(0)
+
+# :splendid is a symbol
+books["Gravity's Rainbow"] = :splendid
+
+puts books.keys
+puts books.values
+
+books.keys.each { |title| puts title }
+books.each { |title, symbol| print title, " => ", symbol, "\n"}
--- examples/ruby/strings.rb
+++ examples/ruby/strings.rb
@@ -4,3 +4,6 @@
 "Cliffy".reverse # => "yffilC"
 "Cliffy".length  # => 6
 "Cliffy" * 5     # => "CliffyCliffyCliffyCliffyCliffy"
+
+# to_s (to-string method)
+40.to_s.reverse  # => 04
